d34f9fcb2876c38e2dc8dcda441fda6196d1b089
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-02 05:56:24 +0000

    conversion_of_datatypes

---
+++ 01_basics/01_conversionOperations.js
@@ -0,0 +1,23 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)  //converting into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn) //first letter conversion m hmesa capital
+console.log(booleanIsloggedIn);
+
+let someNumber = 33
+
+let stringnumber = String(someNumber)
+console.log(typeof stringnumber);

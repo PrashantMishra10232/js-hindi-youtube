546d4ee0e9d7f4cb680100596ebf350c6a906db6
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-02 05:39:11 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using node.js not browser
+
+console.log(3+3)
+
+let name = "prashant"
+let age  = 18
+let isloggedIn = true
+let state = null
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => ture/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+console.log(typeof null);

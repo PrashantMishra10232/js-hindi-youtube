222f48570dff5881059f340c175835b5f21bf395
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-02 06:35:18 +0000

    conversionOperations

--- 01_basics/01_conversionOperations.js
+++ 01_basics/01_conversionOperations.js
@@ -1,23 +1,60 @@
-let score = undefined
+// let score = undefined
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)  //converting into number
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)  //converting into number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isloggedIn = 1
+// let isloggedIn = 1
 
-let booleanIsloggedIn = Boolean(isloggedIn) //first letter conversion m hmesa capital
-console.log(booleanIsloggedIn);
+// let booleanIsloggedIn = Boolean(isloggedIn) //first letter conversion m hmesa capital
+// console.log(booleanIsloggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringnumber = String(someNumber)
-console.log(typeof stringnumber);
+// let stringnumber = String(someNumber)
+// console.log(typeof stringnumber);
+
+
+//********************** operations ***********************
+
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+//console.log(2**2); //power k liye (2 raise to the power 2)
+ //console.log(2**3); //power k liye (2 raise to the power 3)
+// console.log(2/2);
+// console.log 
+
+let str1 = "hello"
+let str2 = " prashant"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2); 
+// console.log("1" + 2 + 2); //agr pehle string h toh piche walo ko string m convert kr denge
+// console.log(1 + 2 + " 2"); //wrna normally pehle solve phir string se add krne k liye conversion hoga
+
+// console.log(+true);
+// console.log(+""); //("") isko boolean m false mante h
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);

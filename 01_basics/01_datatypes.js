f9897c5bbb5b4b6017e3c99003c473c8b254f685
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-03 15:51:12 +0000

    datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -4,16 +4,41 @@
 
 console.log(3+3)
 
-let name = "prashant"
-let age  = 18
-let isloggedIn = true
-let state = null
+
+
+
+/*primitive datatype (call by value)*/
 
 // number => 2 to power 53
+let age  = 18
+let age2 = 100.4
 // bigint
+let bigNumber = 34564789452356n
 //string => ""
+let name = "prashant"
 //boolean => ture/false
+let isloggedIn = true
 // null => standalone value
+let state = null
 // undefined =>
+let userEmail;
+let userEmail2 = undefined
 // symbol => unique
-console.log(typeof null);
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+/*reference (non primitive datatype)*/
+
+//array, objects, functions
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "prashant",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myObj); //to check the type of any datatype

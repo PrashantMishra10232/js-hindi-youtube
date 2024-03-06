cba0f50ce43bd5f06ecb7491b7835d0c0c52dceb
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-06 17:14:21 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "prashant"
+const repoCount = 50
+
+//console.log(name + repoCount + " " + "value");  //do not use it
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('prashant')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
+console.log(gameName.toUpperCase);
+
+console.log(gameName.toLowerCase);

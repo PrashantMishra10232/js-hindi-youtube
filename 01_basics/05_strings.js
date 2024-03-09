f6ebb1a9e5188944ad00b1a66f08f7dcb7c0de3f
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-03-09 07:32:33 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,11 +7,34 @@ console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('prashant')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
+// console.log(gameName.length);
 
-console.log(gameName.toUpperCase);
+// console.log(gameName.toUpperCase());
 
-console.log(gameName.toLowerCase);
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+const newStringOne = "  prashant  "
+console.log(newStringOne);
+console.log(newStringOne.trim()); 
+
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('hitesh'));
+console.log(url.includes('sundar'));
+
+
+const game = "Assassin's Creed Odyssey"
+console.log(game.split(' ')); //string to array

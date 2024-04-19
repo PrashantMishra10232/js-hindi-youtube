195f074b81f61997afa35f52defb2e33058e7cdc
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-04-19 05:44:57 +0000

    done

--- 04_controlFlow/truthy.js
+++ 04_controlFlow/truthy.js
@@ -4,3 +4,25 @@
 
 //truthy values
 //"0", `false`,    
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("object is empty");
+}
+
+//nullish coalescing operator(??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = undefined ?? 10 ?? 20
+
+// console.log(val1);
+
+// Terinary operator
+
+// condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80  ");

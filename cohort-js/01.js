db5b9c8e6a98ceef49443419a97fe2adc1f13905
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-05-19 07:58:07 +0000

    do

---
+++ cohort-js/01.js
@@ -0,0 +1,37 @@
+console.log("Hello world");
+
+// let firstName = "prashant";
+// let age = 22;
+// let isMarried = true;
+
+// if (isMarried) {
+//     console.log(firstName + " is married");
+// }
+// else(
+//     console.log(firstName + " is not married")
+// )
+
+
+//////////////////////////////////////////////////
+
+// let FirstName = "Prashant"
+// let LastName = "Mishra"
+// console.log("Welcome " + FirstName +" " +  LastName);
+
+//////////////////////////////////////////////////
+
+// let id = "Salman"
+// gender = "female"
+
+// if (gender == "male") {
+//     console.log("hi boy");
+// }
+// else{
+//     console.log("hi girl")
+// }
+
+//////////////////////////////////////////////////
+
+for(let i = 0;i<=1000;i++){
+    console.log(i);
+}

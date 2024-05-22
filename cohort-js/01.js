e37c4b67a5cec9f2caf26a33f3660c8e7b08b077
Prashant Mishra <140933262+PrashantMishra10232@users.noreply.github.com>
2024-05-22 07:36:27 +0000

    yes

--- cohort-js/01.js
+++ cohort-js/01.js
@@ -40,43 +40,107 @@ console.log("Hello world");
 ////////////////////////////////////////////////////
 //multiple functions and call back function
 
-function calculateArithmetic(a,b,type){
-    if (type == "sum") {
-        return a+b;
-    }
-    if (type == "sub") {
-        return a-b;
-    }
-}
+// function calculateArithmetic(a,b,type){
+//     if (type == "sum") {
+//         return a+b;
+//     }
+//     if (type == "sub") {
+//         return a-b;
+//     }
+// }
 
-function sum(a,b){
-    return a+b;
-}
-function sub(a,b){
-    return a-b;
-}
+// function sum(a,b){
+//     return a+b;
+// }
+// function sub(a,b){
+//     return a-b;
+// }
 // const value = calculateArithmetic(1,2,"sub");
 // console.log(value);
 
 // now lets do it as a call back function and lets understand it
 
-function calculatesum(a,b,sumOfIt){
-    const ans = sumOfIt(a,b)
-        return ans;
+// function calculatesum(a,b,sumOfIt){
+//     const ans = sumOfIt(a,b)
+//         return ans;
     
-}
-function sum(a,b){
-    return a+b;
-}
+// }
+// function sum(a,b){
+//     return a+b;
+// }
 
-const value = calculatesum(1,2,sum);
-console.log(value);
+// const value = calculatesum(1,2,sum);
+// console.log(value);
 
 ////////////////////////////////////////////
 
-function greet(){
-    console.log(" hello world");
-}
+// function greet(){
+//     console.log(" hello world");
+// }
 
-setTimeout(greet,1*1000); //jo isme second argument h woh time decide kr ra h ki kitne time baad first argument ko call krenge its also a call back function
+// setTimeout(greet,1*1000); //jo isme second argument h woh time decide kr ra h ki kitne time baad first argument ko call krenge its also a call back function
 // setInterval(greet,1*1000)
+
+
+/////////////////////////////////////////////////////////////////////////
+// function findIndexOf (str,target){
+//     console.log("orignal string:", str);
+//     console.log("length:", str.length);
+//     console.log("Index:", str.indexOf(target));
+//     console.log("Index:", str.lastIndexOf(target));
+// }
+
+// findIndexOf("hello world world world", "world")
+
+///////////////////////////////////////////////////////////////////////
+// function getSlice(str, start, end){
+//     console.log("orignal string:", str);
+//     console.log("After slice:",str.slice(start,end));
+// }
+
+// getSlice("helloworld",0,5)
+
+// //////////////////
+// let ans = "tharkiratsingh".slice(2,6);
+// let ans2 = "tharkiratsingh".substr(2,6); //yeh jo second argument h yeh length of substring puchta h index se iska koi lena dena nhi h
+// console.log(ans);
+// console.log(ans2);
+
+// ////////////////////
+// function cutIt(str, startIndex, endIndex){
+//     let newStr = "";
+//     for (let i = 0; i < str.length; i++) {
+//         if(i>=startIndex && i<=endIndex){
+//             newStr = newStr +str[i];
+//         }
+        
+//     }
+//     return newStr;
+// }
+// const value = "Prashant Mishra"
+// console.log(cutIt(value, 1,5));
+
+
+////////////////////////////////replace string//////////////////////////////////////////
+// function replacString(str,target,replacement){
+//     console.log("originalString:",str);
+//     console.log(str.replace(target,replacement));
+// }
+
+// const str = "hello world"
+// console.log(str.replace("world","javaScript"));
+
+// function splitString(str, separator){
+//     console.log("originalString:",str);
+//     console.log(str.split(separator));
+// }
+// const str = "hello world"
+// const value = "hello world"
+// const words = value.split(" ");
+// console.log(words);
+// console.log(splitString("hello world", " "));
+
+/////////////////////////////////////////////////////////////
+
+
+
